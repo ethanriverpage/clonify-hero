b096fdacaff6954481f1d11e20fa28d2eadacd15
ethanriverpage <ethanriverpage@gmail.com>
2022-12-10 19:35:39 -0500

    This hurts

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import spotipy
 import requests
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
+import urllib.request
+import urllib.parse
+import cgi
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.http import MediaIoBaseDownload
+import re
+import io
 
 load_dotenv()
 
@@ -11,10 +21,20 @@ load_dotenv()
 client_id = os.getenv("SPOTIFY_CLIENT_ID")
 client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
+creds = service_account.Credentials.from_service_account_file("service_account.json")
+if True:
+    flow = InstalledAppFlow.from_client_secrets_file(
+        "client_secrets.json", scopes=["https://www.googleapis.com/auth/drive.file"]
+    )
+    creds = flow.run_console()
+
+creds_dict = json.loads(creds.to_json())
+creds = Credentials.from_authorized_user_info(info=creds_dict)
+
 
 def SpotifyData():
     # The URL of the playlist you want to get the songs from
-    playlist_url = "https://open.spotify.com/playlist/10Attpbti1MIUH2Og2IgIv"
+    playlist_url = input("Paste the Spotify playlist URL here: ")
 
     # Use the SpotifyClientCredentials class to authenticate with the Spotify API
     client_credentials_manager = SpotifyClientCredentials(
@@ -23,7 +43,8 @@ def SpotifyData():
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
     # Parse the playlist URL to get the playlist's ID
-    playlist_id = playlist_url.split("/")[-1]
+    playlist_id = playlist_url.split("?")[0].split("/")[4]
+    print(playlist_id)
 
     # Use the Spotify API to get the tracks in the playlist
     results = sp.playlist_tracks(playlist_id)
@@ -44,10 +65,11 @@ def SpotifyData():
 
 
 def FileCheck():
-    if os.path.exists("response.json"):
-        overwrite = input("File already exists! Would you like to overwrite it? (y/n)")
+    if os.path.exists("response.json") or os.path.exists("songs.json"):
+        overwrite = input("File already exists! Would you like to overwrite it? (y/n) ")
         if overwrite == "y":
             os.remove("response.json")
+            os.remove("songs.json")
             print("Overwriting...")
         else:
             print("Skipping overwrite...")
@@ -84,8 +106,11 @@ def ChorusAPIQuery():
                         "directLinks": response.json()["songs"][i]["directLinks"],
                     }
                 }
-
-            existing_data["songs"].append(new_song)
+                # How to get this to check if song already exists in existing_data to avoid extra API queries?
+                print(f"Found song: " + new_song["songs"]["name"])
+                existing_data["songs"].append(new_song)
+        else:
+            print("...")
 
         # Write the combined data back to the file
         with open("response.json", "w") as f:
@@ -96,13 +121,68 @@ def ChorusAPIParse():
     with open("response.json") as f:
         api_response = json.load(f)
 
+    if not os.path.exists("songs"):
+        os.makedirs("songs")
+
     for song in api_response["songs"]:
-        print(f"Name: {song['songs']['name']}")
-        print(f"Link: {song['songs']['link']}")
-        print(f"DL: {song['songs']['directLinks']['archive']}")
+        try:
+            url = song["songs"]["directLinks"]["archive"]
+            remotefile = urllib.request.urlopen(url)
+            remotefile_info = remotefile.info()["Content-Disposition"]
+            value, params = cgi.parse_header(remotefile_info)
+            file_name = params["filename"]
+            file_path = os.path.join("songs", file_name)
+            try:
+                if not os.path.exists(file_path):
+                    urllib.request.urlretrieve(url, file_path)
+                    print(f"Song downloaded: {song['songs']['name']}")
+                else:
+                    print(f"Already downloaded: {song['songs']['name']}")
+            except Exception as e:
+                print(f"Failed to download: {song['songs']['name']}: {e}")
+        except:
+            for key in song["songs"]["directLinks"]:
+                os.makedirs(
+                    os.path.join("songs", (song["songs"]["name"])), exist_ok=True
+                )
+                url = song["songs"]["directLinks"][key]
+
+                if not "google" in url:
+                    try:
+                        remotefile = urllib.request.urlopen(url)
+                        remotefile_info = remotefile.info()["Content-Disposition"]
+                        value, params = cgi.parse_header(remotefile_info)
+                        file_name = params["filename"]
+                        file_path = os.path.join("songs", (song["songs"]["name"]))
+                        file_path_join = os.path.join(file_path, file_name)
+                        try:
+                            if not os.path.exists(file_path_join):
+                                urllib.request.urlretrieve(url, file_path_join)
+                                print(f"File downloaded: {file_path}")
+                            else:
+                                print(f"Already downloaded: {song['songs']['name']}")
+                        except Exception as e:
+                            print(f"Failed to download: {song['songs']['name']}: {e}")
+                    except Exception as e:
+                        print(f"Invalid URL: " + url)
+                        print(f"Exception: {e}")
+                else:
+                    service = build("drive", "v3", credentials=creds)
+                    file_id = re.split(r"=|&", url)
+                    print(file_id)
+                    request = service.files().get_media(fileId=file_id[1])
+                    fh = io.BytesIO()
+                    downloader = MediaIoBaseDownload(fh, request)
+                    done = False
+                    while done is False:
+                        status, done = downloader.next_chunk()
+                        print("Download %d%%." % int(status.progress() * 100))
+
+        # print(f"Link: {song['songs']['link']}")
+        # print(f"DL: {song['songs']['directLinks']['archive']}")
 
 
-SpotifyData()
 FileCheck()
+SpotifyData()
 ChorusAPIQuery()
 ChorusAPIParse()
